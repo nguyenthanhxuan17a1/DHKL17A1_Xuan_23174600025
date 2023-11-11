e90822236677e9d4c6503527b23b821a258e529d
nguyenthanhxuan17a1 <148075236+nguyenthanhxuan17a1@users.noreply.github.com>
2023-11-11 08:56:42 +0700

    Add files via upload

---
+++ 7.7.py
@@ -0,0 +1,14 @@
+tien_muon_doi=1375000
+so_to_1=tien_muon_doi//500000
+tien_con_lai=tien_muon_doi%500000
+so_to_2=tien_muon_doi//200000
+tien_con_lai=tien_con_lai%200000
+so_to_3=tien_muon_doi//100000
+tien_con_lai=tien_muon_doi%100000
+so_to_4=tien_muon_doi//50000
+tien_con_lai=tien_con_lai%50000
+print('số tiênf muốn đổi:',tien_muon_doi)
+print('số tờ 500000:',so_to_1)
+print('số tờ 200000:',so_to_2)
+print('số tờ 100000:',so_to_3)
+print('số tờ 50000:',so_to_4)
